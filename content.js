4d2287aeb40946e5d51c2dae469715d0b744c6f7
Arash Rai <arashrai17@gmail.com>
2015-10-04 19:01:42 -0400

    Create content.js

---
+++ content.js
@@ -0,0 +1,10 @@
+var links = $("a[href^='http']");
+
+var firstHref = links.eq(Math.floor(Math.random()*links.length)).attr("href");
+
+var stuff = $('body').html()
+
+if(stuff.indexOf('Hitler') == -1){
+chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});}
+
+setTimeout(function(){chrome.tabs.create({"url": request.url})},3242389492200);
