d1128babf486952faebae6ce2e83ce4f4674d767
Arash Rai <arashrai17@gmail.com>
2016-01-16 19:00:20 -0500

    Update content.js

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ var firstHref = links.eq(Math.floor(Math.random()*links.length)).attr("href");
 
 var stuff = $('body').html()
 
-if(stuff.indexOf('Hitler') == -1){
+if(stuff.indexOf('(Given Word))') == -1){
 chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});}
 
 setTimeout(function(){chrome.tabs.create({"url": request.url})},3242389492200);
